e3e85b963cdae5f3ae16c6cd668d21c22c61fcf6
Lombard97 <lombard_k-po@hotmail.com>
2017-06-28 21:56:56 -0300

    lalala

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+//sobreescribe la variable
+	var dato=0;
+	dato="Alejandro";
+	alert(dato);
+
+	// dato=document.getElementById('elNombre').value;
+	// alert(dato);
+	document.getElementById('elNombre').value= "natalia natalia"
+}
+
+
+
