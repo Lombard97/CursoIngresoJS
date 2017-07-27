fb62942c9b1698c94fd60e8cd230d44ca1404add
Lombard97 <lombard_k-po@hotmail.com>
2017-07-26 22:06:32 -0300

    ejercicios del parcial hasta el 4

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+	var numero1;
+	var numero2;
+	var multiplicacion;	
+	var resta;
+	numero1 = parseInt(prompt("ingrese el primer numero"));
+	numero2 = parseInt(prompt("ingrese el segundo numero"));
 
+		if(numero1 == numero2)
+		{
+			multiplicacion = numero1*numero2;
+		}
+	alert("la multiplicacion es igual: "+multiplicacion);
+
+			if(numero1>numero2)
+			{
+				resta = numero1 - numero2;
+			}
+
+	alert("La resta es igual: "+resta);
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,17 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+	largo = parseInt(document.getElementById('alrgo').value);
+	ancho = parseInt(document.getElementById('ancho').value);
+
+
+	perimetro = largo*2+ancho*2;
+
+	alambre = perimetro*3;
+
+	alert("El alambre necesario es: "+alambre+" metros");
 
 }
